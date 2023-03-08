01424af1a7076f160242b15fd8b60951d22c7df1
reginyuan <2496943860@qq.com>
2023-03-08 16:00:43 +0800

    修改环境参数

--- server/api/user.ts
+++ server/api/user.ts
@@ -1,6 +1,12 @@
 import { fetchTMDB } from '~/composables/utils/tmdb'
 // 获取用户详细信息
 export function getInfo(params: any) {
+  /**
+   *参数
+   *接口 string
+   *配置请求方式和请求头数据 object
+   *传递数据 object
+   */
   return fetchTMDB(
     '/system/notice/list',
     {
