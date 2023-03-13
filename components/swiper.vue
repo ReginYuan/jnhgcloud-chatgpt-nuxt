d067783483569b6ac148e93a665f5b047fed0378
shihuan01 <2593945796@qq.com>
2023-03-13 11:14:20 +0800

    fix:优化样式

--- components/swiper.vue
+++ components/swiper.vue
@@ -1,38 +1,23 @@
 <template>
   <div class="swiperbox">
     <swiper
-      :width="220"
+      :width="230"
       :slidesPerView="auto"
       :centeredSlides="true"
       :spaceBetween="15"
       :slidesOffsetBefore="0"
       :loop="true"
-      :pagination="{
-        clickable: true
-      }"
-      :navigation="true"
+      :loopAdditionalSlides="3"
       class="mySwiper"
+      :modules="modules"
     >
-      <swiper-slide
-        ><img
-          src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
-          alt=""
-      /></swiper-slide>
-      <swiper-slide
-        ><img
-          src="https://www.hao528.com/uploads/allimg/220605/9-220605160242.jpg"
-          alt=""
-      /></swiper-slide>
-      <swiper-slide
-        ><img
-          src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
-          alt=""
-      /></swiper-slide>
-      <swiper-slide
-        ><img
-          src="https://www.hao528.com/uploads/allimg/220605/9-220605160242.jpg"
-          alt=""
-      /></swiper-slide>
+      <swiper-slide v-for="(item, index) in imgs" :key="index">
+        <div class="content">
+          <img :src="item.url" alt="" />
+          <div class="title">俄乌谈判乌方代表：乌方不再致力于申请加入北约</div>
+          <div class="tagInfo">央视新间客户端·10分钟前</div>
+        </div>
+      </swiper-slide>
     </swiper>
   </div>
 </template>
@@ -55,11 +40,19 @@ import 'swiper/css/pagination'
 // };
 // setup语法糖只需要这样创建一个变量就可以正常使用分页器和对应功能，如果没有这个数组则无法使用对应功能
 const modules = [Autoplay, Pagination, Navigation, A11y]
+
+const imgs = ref([
+  { url: 'https://www.hao528.com/uploads/allimg/220605/9-220605160242.jpg' },
+  { url: 'https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg' },
+  { url: 'https://www.hao528.com/uploads/allimg/220605/9-220605160242.jpg' },
+  { url: 'https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg' }
+])
 </script>
 
 <style scoped lang="scss">
 .swiper {
-  height: 125px !important;
+  height: 145px !important;
+  margin: 7px 0px 10px 16px;
 }
 :deep(.swiper-button-prev:after) {
   content: none !important;
@@ -67,13 +60,28 @@ const modules = [Autoplay, Pagination, Navigation, A11y]
 :deep(.swiper-button-next:after) {
   content: none !important;
 }
-:deep(.swiper-slide .swiper-slide-active) {
-  width: 220px !important;
-}
-img {
+.content {
   width: 100%;
-  // width: 60vw;
-  height: 100%;
-  border-radius: 10px;
+  height: 80%;
+  position: relative;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+  }
+  .title {
+    position: absolute;
+    bottom: 8px;
+    font-size: 16px;
+    color: #ffffff;
+    padding: 0 8px;
+  }
+  .tagInfo {
+    z-index: 99999999999999 !important;
+    position: absolute;
+    bottom: -25px;
+    font-size: 10px;
+    color: rgba($color: #000000, $alpha: 0.5);
+  }
 }
 </style>
--- pages/IndustryReport/index.vue
+++ pages/IndustryReport/index.vue
@@ -1,7 +1,122 @@
 <template>
-  <div></div>
+  <!-- 头部搜索区域 -->
+  <div class="fixd">
+    <div class="header">
+      <van-nav-bar left-text="行业报告" left-arrow :clickable="false">
+        <template #right>
+          <van-search
+            v-model="value"
+            placeholder="搜索资讯"
+            background="transparent"
+            shape="round"
+          >
+            <template #right-icon>
+              <img src="~/assets/img/icon-search.png" alt="" />
+            </template>
+          </van-search>
+        </template>
+      </van-nav-bar>
+    </div>
+    <van-tabs v-model:active="active" line-height="0">
+      <van-tab :title="item" v-for="(item, index) in tabList" :key="index">
+      </van-tab>
+    </van-tabs>
+  </div>
 </template>
+<script lang="ts" setup>
+import { ItemListType } from '~/types/itemList'
+import { ref, onMounted } from 'vue'
+const active = ref(0)
+let tabList = ref([''])
 
-<script setup lang="ts"></script>
+let value = ref('')
 
-<style scoped lang="scss"></style>
+tabList.value = [
+  '推荐',
+  '资本投资',
+  '建设单位',
+  '设计院',
+  '材料设计院',
+  '推荐',
+  '资本投资',
+  '建设单位',
+  '设计院',
+  '材料设计院',
+  '推荐',
+  '资本投资',
+  '建设单位',
+  '设计院',
+  '材料设计院',
+  '推荐',
+  '资本投资',
+  '建设单位',
+  '设计院',
+  '材料设计院'
+]
+</script>
+
+<style scoped lang="scss">
+.fixd {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 99;
+}
+.header {
+  background: url('~/assets/img/hand-bg-green.png') no-repeat;
+  background-size: 100% 100%;
+  :deep(.van-nav-bar) {
+    background-color: transparent;
+    font-size: 20px;
+    .van-nav-bar__right {
+      width: 199px;
+      margin-left: 31px;
+      .van-search {
+        padding: 0;
+        .van-field__right-icon {
+          display: flex;
+          align-items: center;
+        }
+      }
+    }
+    .van-nav-bar__arrow {
+      font-size: 20px;
+      color: #ffffff;
+    }
+    .van-nav-bar__text {
+      font-size: 20px;
+      color: #ffffff;
+      margin-left: 10px;
+    }
+    .van-field__left-icon {
+      display: none;
+    }
+  }
+}
+
+:deep(.van-tab__text) {
+  color: #888888;
+  font-size: 16px;
+}
+:deep(.van-tab--active) {
+  .van-tab__text {
+    position: relative;
+    color: #000000;
+    &::after {
+      content: '';
+      width: 10px;
+      height: 5px;
+      border-radius: 7px 7px 0 0;
+      background-color: #2ac670;
+      position: absolute;
+      bottom: -8px;
+      left: 38%;
+    }
+  }
+}
+.content {
+  height: calc(100vh - 100px);
+  overflow: auto;
+}
+</style>
