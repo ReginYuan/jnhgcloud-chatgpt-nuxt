638e227267aa2110bd5ded08cc09a4a2baf97e2b
reginyuan <2496943860@qq.com>
2023-03-31 10:22:26 +0800

    feat: 修改对应数据

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
 export default defineNuxtConfig({
+  build: {
+    // transpile: ['@pzy915/pdf-preview']
+  },
+  alias: {
+    // '@pzy915/pdf-preview': '@pzy915/pdf-preview/dist/pdf-preview.umd.cjs'
+  },
   // 把env放入这个里面，通过useRuntimeConfig获取
   vite: {
     envDir: '~/env', // 指定env文件夹
     optimizeDeps: {
       // 用于：从预捆绑中排除的依赖项
       exclude: []
-    }
+    },
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: true
+      }
+    },
+    plugins: [viteCommonjs(), esbuildCommonjs()]
   },
   css: ['@/assets/base.ignore.css'],
   postcss: {
